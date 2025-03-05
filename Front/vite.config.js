ff096e9b3408c2b92014dea0b6fca6eb18725734
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-04 20:55:55 -0600

    Update vite.config.js

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',   // Asegura que Vite escuche en todas las interfaces
+    port: process.env.PORT || 5173,  // Usa el puerto de Render o 5173 como fallback
+  },
 })
