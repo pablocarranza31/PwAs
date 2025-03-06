4f861082176b7591e7160b52f28555052a02e60e
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-06 17:14:58 -0600

    splash

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './components/Register';
@@ -9,18 +9,28 @@ import SplashScreen from './components/SplashScreen';
 function App() {
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    // Simula la carga de recursos o la inicialización de la PWA
+    setTimeout(() => {
+      setLoading(false); // Después de 3 segundos, cambia el estado a false
+    }, 3000);
+  }, []);
+
   return (
     <div className="p-4">
-      
+      {/* Si está cargando, muestra el SplashScreen */}
+      {loading ? (
+        <SplashScreen />
+      ) : (
         <Router>
           <Routes>
-            <Route path="/" element={<Home />} /> 
+            <Route path="/" element={<Home />} />
             <Route path="/register" element={<Register />} />
             <Route path="/login" element={<Login />} />
             <Route path="/main" element={<Main />} />
           </Routes>
         </Router>
-    
+      )}
     </div>
   );
 }
