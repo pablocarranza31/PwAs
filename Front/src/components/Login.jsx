ce1d767959c225261eb438fd6be4906e317b8915
PabloCarranza31 <158582000+PabloCarranza31@users.noreply.github.com>
2025-03-07 12:18:14 -0600

    implementacion axios

--- Front/src/components/Login.jsx
+++ Front/src/components/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../App.css'; // Archivo CSS para los estilos
+import axios from 'axios';
+import { json } from 'body-parser';
 
 function Login() {
   const navigate = useNavigate();
@@ -12,17 +14,27 @@ function Login() {
     e.preventDefault();
 
     try {
+      const opcions = {
+        method: 'POST',
+        url:'https://pwasb.onrender.com/api/subs/login',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        data:{ email, password },
+      }
+      const data = await axios.request(opcions);
+     /* 
       const response = await fetch('https://pwasb.onrender.com/api/subs/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({ email, password }),
-      });
+      });*/
  
-      const data = await response.json();
+      //const data = await response.json();
 
-      if (response.ok) {
+      if (response) {
       // Guardar solo el ID del usuario en localStorage
       localStorage.setItem('userId', data.user._id);
         alert('Inicio de sesión exitoso');
